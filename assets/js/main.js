d61c2dd3d631a65166111b6912b215558b4ad81c
Greig McMahon <greig@gekkocreations.com.au>
2017-06-08 12:01:56 +1000

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+/* login home page */
+
+$(document).ready(function() {
+  $('#olvidado').click(function(e) {
+    e.preventDefault();
+    $('div#form-olvidado').toggle('500');
+  });
+  $('#acceso').click(function(e) {
+    e.preventDefault();
+    $('div#form-olvidado').toggle('500');
+  });
+});
+
+
+/* login home page */
+
+$(document).ready(function() {
+  $('#olvidado1').click(function(e) {
+    e.preventDefault();
+    $('div#form-olvidado1').toggle('500');
+  });
+  $('#acceso1').click(function(e) {
+    e.preventDefault();
+    $('div#form-olvidado1').toggle('500');
+  });
+});
+
+
+
+/* menu slider */
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+
+$(".image-container").click(function() {
+  window.location = $(this).find("a").attr("href"); 
+  return false;
+});
+
+
+
+var myBarChart = new Chart(ctx).Bar(data, option);
+
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+
